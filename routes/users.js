5f70198fa7db9e69415e73a0453b784ac831ab57
gitvalentina <vdouce@udesa.edu.ar>
2022-03-31 17:57:59 -0300

    controladores para users

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var controlador= require('../controllers/usersController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
-});
-router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: 'Express' });
-});
-router.get('/profile-edit', function(req, res, next) {
-  res.render('profile-edit', { title: 'Express' });
-});
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' });
-});
+router.get('/login', controlador.login);
+router.get('/profile', controlador.profile );
+router.get('/profile-edit', controlador.profileEdit);
+router.get('/register', controlador.register);
 
 module.exports = router;
