b25ad80d97ae6959b75860eb110f0e320b98af9e
franciscotaylor10 <ftaylor@udesa.edu.ar>
2022-04-11 14:48:11 -0300

    database

---
+++ db/data.js
@@ -0,0 +1,23 @@
+const data = {
+    user: { username: 'user'},
+    products: [
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'},
+        {desription: 'product 1', photo: '/images/product1.jpg'}
+    ],
+    coments: [
+        {content: 'comment 1', username: 'user'},
+        {content: 'comment 1', username: 'user'},
+        {content: 'comment 1', username: 'user'},
+        {content: 'comment 1', username: 'user'},
+        {content: 'comment 1', username: 'user'}
+    ],
+}
